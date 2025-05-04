a9e8cffe9644103361675cb36eb4a91b311ae91f
miho-imakawa <mihou.imakawa@gmail.com>
2025-05-04 21:36:03 +0900

    Add files via upload

--- sample.py
+++ sample.py
@@ -3,6 +3,12 @@ import time
 
 st.title('streamlit 入門')
 
+##カラムの使い方　columnsに入れると、その数のカラムができる
+left_column, right_column = st.columns(2)
+button = left_column.button('このボタンで右カラムに文字が表示されます。')
+if button:
+    right_column.write('右カラムに文字が表示されたよ')
+
 st.write('ブログレスバーの表示')
 
 ##　プログレスバーの使い方
@@ -10,7 +16,7 @@ latest_iteration = st.empty()
 bar = st.progress(0)
 
 for i in range(100):
-    latest_iteration.text(f'Iteration {i+1}')
+    latest_iteration.text(f'進捗具合 {i+1}')
     bar.progress(i + 1)
     time.sleep(0.1)
 'DONE!'
@@ -21,9 +27,5 @@ expander.write('問い合わせの回答を記入')
 expander1 = st.expander('問い合わせ1')
 expander1.write('問い合わせの回答を記入1')
 
-##カラムの使い方　columnsに入れると、その数のカラムができる
-left_column, right_column = st.columns(2)
-button = left_column.button('右カラムに文字を表示')
-if button:
-    right_column.write('右カラムに文字を表示')
+
 
